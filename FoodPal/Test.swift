b7bccf895c8a1463f143026f5d4915e0b19eef97
Phila <phila.dlamini@tufts.edu>
2024-06-24 00:24:51 +0200

    create screen redesign attempt

--- FoodPal/Test.swift
+++ FoodPal/Test.swift
@@ -9,17 +9,106 @@ import SwiftUI
 import CoreLocation
 
 struct Test: View {
-    
-    @State var countries = [Country]()
+    @State var images = [UIImage]()
+    @State var title = ""
+    @State var description = ""
+    @State var showImagePicker = false
+    @State var pickFromLibrary = true
+    @State var expiryDate = Date.now 
 
     var body: some View {
         
-        Text("Hi")
-            .onAppear {
-                getAddress(for: CLLocation(latitude: 12.521110, longitude: -69.968338)) {add in
-                    print("country: \(add.country) region: \(add.region) city: \(add.city)")
+        VStack(alignment: .leading, spacing: 10) {
+            HStack(alignment: .top) {
+                
+                Circle() //user pic
+                    .fill(.gray)
+                    .frame(width: 35)
+                
+                VStack (alignment: .leading, spacing: 10) {
+                    TextField("Food name", text: $title)
+                        .bold()
+                    
+                    TextField("Desribe food", text: $description, axis: .vertical)
+                    
+                    
+                    
                 }
+               
+                
             }
+            
+                if !images.isEmpty {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0..<images.count, id: \.self) {index in
+                                let image = images[index]
+                                
+                                ZStack(alignment: .topTrailing) {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 130, height: 200)
+                                        .cornerRadius(10)
+                                    
+                                    Image(systemName: "xmark.circle.fill")
+                                        .renderingMode(.template)
+                                        .onTapGesture {
+                                            print("about to remove image at index")
+                                            images.remove(at: index)
+                                            print("removed image at index")
+                                        }
+                                        .padding(8)
+                                }
+                            }
+                        }
+                        .padding(.leading, 40)
+                        
+                    }
+                    
+                }
+                
+                
+            VStack(alignment: .leading, spacing: 10) {
+
+                HStack {
+                    Image(systemName: "photo.on.rectangle.angled")
+                        .foregroundColor(.gray)
+                        .onTapGesture {
+                            pickFromLibrary = true
+                            showImagePicker = true
+                        }
+                    
+                    Image(systemName: "camera.fill")
+                        .foregroundColor(.gray)
+                        .onTapGesture {
+                            pickFromLibrary = false
+                            showImagePicker = true
+                        }
+                    
+                }
+                
+                Text("At least 3 images required")
+                    .font(.caption)
+            }
+            .padding(.leading, 40)
+            
+        }
+        .toolbar {
+            ToolbarItem(placement: .keyboard) {
+                DatePicker("Expiry date", selection: $expiryDate, in: Date()..., displayedComponents: .date)
+            }
+        }
+        .sheet(isPresented: $showImagePicker) {
+            if pickFromLibrary {
+                ImagePicker(images: $images, isPickerShowing: $showImagePicker, sourceType: .photoLibrary)
+            } else {
+                ImagePicker(images: $images, isPickerShowing: $showImagePicker, sourceType: .camera)
+            }
+        }
+        
+        
+        
     }
    
 }
